92185f2997b3eb6fae7022f9e7f2ced419833e0c
Yuri Astrakhan <yuriastrakhan@gmail.com>
2025-10-13 04:10:18 -0400

    chore: fix parse case, use test_each_file (#6)

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -2,47 +2,75 @@
 
 use std::borrow::Cow;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use can_dbc_pest::{decode_cp1252, DbcParser, Rule};
 use insta::{assert_debug_snapshot, with_settings};
 use pest::Parser;
+use test_each_file::test_each_path;
+
+struct TestConfig {
+    test_root: &'static str,
+    snapshot_suffix: &'static str,
+    use_cp1251: bool,
+    create_snapshot: bool,
+}
+
+static TEST_DIRS: &[TestConfig] = &[
+    TestConfig {
+        test_root: "shared-test-files",
+        snapshot_suffix: "",
+        use_cp1251: true,
+        create_snapshot: true,
+    },
+    TestConfig {
+        test_root: "opendbc/opendbc/dbc",
+        snapshot_suffix: "opendbc",
+        use_cp1251: false,
+        create_snapshot: false,
+    },
+];
+
+test_each_path! { in "./tests/fixtures/opendbc/opendbc/dbc" as dbc => parse_one_file }
+test_each_path! { in "./tests/fixtures/shared-test-files" as shared => parse_one_file }
+
+/// Get `test root`, `snapshot name suffix`, `use cp1251`, `create snapshot` for the given path
+fn get_test_info(path: &Path) -> Option<(PathBuf, &'static TestConfig)> {
+    if !path
+        .extension()
+        .unwrap_or_default()
+        .eq_ignore_ascii_case("dbc")
+    {
+        return None;
+    }
+    let path_str = path.to_str().unwrap();
+    let parent = path.parent().unwrap();
+    for item in TEST_DIRS {
+        // Ensure slashes are there for easier matching
+        let test_root = format!("/{}/", item.test_root);
+        let mut path_dir = parent.to_str().unwrap().to_string();
+        if !path_dir.ends_with('/') {
+            path_dir.push('/');
+        }
+        if let Some(pos) = path_dir.find(&test_root) {
+            let parent = PathBuf::from("snapshots")
+                .join(item.snapshot_suffix)
+                .join(&path_dir[pos + test_root.len()..]);
+            return Some((parent, item));
+        }
+    }
+    panic!("Unknown test directory: {path_str}");
+}
 
 /// Test parsing all DBC files
 #[test]
-fn test_cantools_dbc_files() {
-    let test_dirs = [
-        (
-            "tests/shared-test-files/dbc-cantools",
-            "dbc-cantools",
-            true,
-            true,
-        ),
-        ("tests/shared-test-files/canpy", "canpy", true, true),
-        ("tests/opendbc/opendbc/dbc", "opendbc", false, false),
-    ];
-    for (path, file_name, snapshot, cp1251) in test_dirs {
-        let path = Path::new(path);
-        let snapshot_path = String::from("snapshots-") + file_name;
-        // snapshots should go to /tests/snapshots-* directory
-        with_settings! {
-            { omit_expression => true,
-              snapshot_path => snapshot_path ,
-              prepend_module_to_snapshot => false },
-            {
-                test_dbc_files(path, snapshot, cp1251);
-            }
-        }
-    }
-}
-
-/// Test parsing all DBC files in the given directory.
-fn test_dbc_files(dir: impl AsRef<Path>, snapshot: bool, cp1251: bool) {
-    let dir = dir.as_ref();
-    let dir_display = dir.display();
-    let dir_content = fs::read_dir(dir).unwrap_or_else(|e| {
-        panic!(
-            "
+fn test_if_submodules_are_present() {
+    for test in TEST_DIRS {
+        let dir = Path::new("./tests/fixtures").join(test.test_root);
+        fs::read_dir(&dir).unwrap_or_else(|e| {
+            let dir_display = dir.display();
+            panic!(
+                "
 --------------------------------------------------------------------------
 Error reading dbc test files from   {dir_display}
 {e}
@@ -50,28 +78,29 @@ Make sure git submodules are up to date by running
     git submodule update --init --recursive
 --------------------------------------------------------------------------
 "
-        )
-    });
-
-    eprintln!("Testing dbc files in directory: {dir_display}");
-    for dbc_path in dir_content {
-        let path = dbc_path.unwrap().path();
-        if path
-            .extension()
-            .is_some_and(|ext| ext.eq_ignore_ascii_case("dbc"))
-        {
-            parse_one_file(path, snapshot, cp1251);
-        }
+            )
+        });
     }
 }
 
 /// Parse a single DBC file and assert a snapshot of the result.
-fn parse_one_file(path: impl AsRef<Path>, snapshot: bool, cp1251: bool) {
+fn parse_one_file(path: impl AsRef<Path>) {
     let path = path.as_ref();
-    eprintln!("Testing DBC file: {}", path.display());
+    let Some((
+        snapshot_path,
+        &TestConfig {
+            use_cp1251,
+            create_snapshot,
+            ..
+        },
+    )) = get_test_info(path)
+    else {
+        return;
+    };
+
     let file_name = path.file_stem().unwrap().to_string_lossy().to_string();
     let buffer = fs::read(path).unwrap();
-    let buffer = if cp1251 {
+    let buffer = if use_cp1251 {
         decode_cp1252(&buffer)
             .unwrap_or_else(|| panic!("Failed to decode {} as cp1252", path.display()))
     } else {
@@ -82,11 +111,20 @@ fn parse_one_file(path: impl AsRef<Path>, snapshot: bool, cp1251: bool) {
     };
 
     match DbcParser::parse(Rule::file, &buffer) {
-        Ok(pairs) => {
+        Ok(result) => {
             if std::env::var("SKIP_INSTA").is_err()
-                && (snapshot || std::env::var("FORCE_INSTA").is_ok())
+                && (create_snapshot || std::env::var("FORCE_INSTA").is_ok())
             {
-                assert_debug_snapshot!(file_name, pairs);
+                with_settings! {
+                    {
+                        omit_expression => true,
+                        snapshot_path => snapshot_path,
+                        prepend_module_to_snapshot => false
+                    },
+                    {
+                        assert_debug_snapshot!(file_name, result);
+                    }
+                }
             }
         }
         Err(e) => panic!("Failed to parse {file_name}.dbc: {e:#?}"),
